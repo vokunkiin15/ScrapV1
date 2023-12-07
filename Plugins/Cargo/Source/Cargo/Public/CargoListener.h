7baf9a76ff5956ec7ec475c52d8ad221c14b6946
vokunkiin15 <mnkaljar@gmail.com>
2023-12-07 07:00:27 +0300

    c6

---
+++ Plugins/Cargo/Source/Cargo/Public/CargoListener.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FCargoListener : public FRunnable
+{
+public:
+
+	FCargoListener();
+	~FCargoListener();
+
+	virtual uint32 Run() override;
+	virtual void Stop() override;
+
+private:
+
+	FRunnableThread* ListenerThread = NULL;
+};
